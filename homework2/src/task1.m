58caef76045228f02e56ccd7fa8ba81adf028fb9
hnamtang <hnamtang@gmail.com>
2025-01-16 15:19:53 +0100

    Template 3 tasks

---
+++ homework2/src/task1.m
@@ -0,0 +1,10 @@
+% Flight Control - TU Berlin
+% WS 24/25
+% HW 2 - Analysis of dynamic aircraft behavior
+% and design of control systems for manual and automatic flight
+%
+% Task 1 - Analysis of eigenbehavior
+clear all; close all; clc
+
+VFTE_state_space;  % System state-space
+
